38a20b3a1c822e3e0e38223031fe966281f523b9
Naval Kishore Kandpal <A1766@AXTRIA.COM>
2021-03-17 13:13:22 +0530

    Updated use code in webpack.dev.js and webpack.prod.js file.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
@@ -22,19 +23,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [
-						 "style-loader",
-						 "css-loader",
-						 {
-						   loader: "sass-loader",
-						   options: {
-										 implementation: require("sass"),
-										 sassOptions: {
-														fiber: require("fibers"),
-													},
-									},
-						 },
-					 ],
+				use: ["style-loader", "css-loader", "sass-loader"],
             },
         ],
     },
