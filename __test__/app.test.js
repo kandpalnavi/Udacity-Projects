8c3ef1d5fe6d5f799f10ae5a8c8603e804446347
Naval Kishore Kandpal <A1766@AXTRIA.COM>
2021-03-18 15:37:25 +0530

    adding project files to repository.

---
+++ __test__/app.test.js
@@ -0,0 +1,13 @@
+import { makePost, updateUI } from '../src/client/js/app'
+
+describe('testing updateUI function to be defined', () => {
+    test('ensure updateUI function exists', () => {
+        expect(updateUI).toBeDefined();
+    })
+})
+
+describe('testing makePost function to be defined', () => {
+    test('ensure makePost function exists', () => {
+        expect(makePost).toBeDefined();
+    })
+})
---
+++ __test__/server.test.js
@@ -0,0 +1,7 @@
+import { app } from '../src/server/server'
+
+describe('app to be defined', () => {
+    test('ensure app exists', () => {
+        expect(app).toBeDefined();
+    })
+})
